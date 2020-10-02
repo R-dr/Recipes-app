c53ecd0a2948a8ddbf0914f633851a7c98641c26
Rodney <Rodney.ditch@hotmail.com>
2020-10-02 16:46:19 +1000

    added app file

---
+++ lib/app.rb
@@ -0,0 +1,11 @@
+require 'terminal-table'
+require 'colorize'
+require 'json'
+require_relative 'api'
+require_relative 'menu'
+require_relative 'recipe_card'
+
+menu = Menu.new
+menu.choices
+
+end
